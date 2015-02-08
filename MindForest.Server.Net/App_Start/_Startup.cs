72468a2f72e55df5bbf0c5b149d512181767299c
art-ist <dev@art-ist.cc>
2015-02-08 17:18:03 +0100

    fix: add Startup.cs

---
+++ MindForest.Server.Net/App_Start/_Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.Mvc;
+
+[assembly: OwinStartup(typeof(MindForest.Auth))]
+
+namespace MindForest {
+
+	public partial class Auth {
+		public Auth() {
+			Auth.Startup();
+		}
+
+		public void Configuration(IAppBuilder app) {
+
+			GlobalFilters.Filters.Add(new HandleErrorAttribute());
+
+			Auth.Configure(app); 
+
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+
+		}
+
+	}
+
+}
